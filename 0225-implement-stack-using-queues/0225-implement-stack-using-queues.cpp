016724b8f59caac1ff15601664cebc5fe177e897
NishantKSinha <149254763+NishantKSinha@users.noreply.github.com>
2024-07-11 18:55:46 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (12.83%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -10,14 +10,15 @@ public:
     void push(int x) {
         q2.push(x);
         while(!q1.empty()){
-            q2.push(q1.front());    q1.pop();
+            q2.push(q1.front()); 
+            q1.pop();
         }
         swap(q1, q2);
     }
     
     /** Removes the element on top of the stack and returns that element. */
     int pop() {
-        int result = top();
+        int result = q1.front();
         q1.pop();
         return result;
     }
