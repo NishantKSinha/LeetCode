794aef8c515fc31991b8386aa50dde67397032ca
NishantKSinha <149254763+NishantKSinha@users.noreply.github.com>
2024-07-14 18:13:08 +0530

    Time: 3 ms (41.47%), Space: 7.8 MB (12.86%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+        vector<string>ans(10);
+        string temp="";
+        int idx = 0;
+        int count =0;
+        while(idx<s.size()){
+            if(s[idx]== ' '){
+                int pos = temp[temp.size()-1]-'0';
+                temp.pop_back();
+                ans[pos] = temp;
+                temp.clear();
+                count++;
+                idx++;
+            }
+            else{
+                temp+=s[idx];
+                idx++;
+            }
+        }
+            //for last word
+              int pos = temp[temp.size()-1]-'0';
+                temp.pop_back();
+                ans[pos] = temp;
+                temp.clear();
+                count++;
+        for(int i = 0;i<=count;i++){
+            if(i == 0) continue;
+            temp+=ans[i];
+            temp+= ' ';
+        }
+        temp.pop_back();
+        return temp;
+    }
+};
