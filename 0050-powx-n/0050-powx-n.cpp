872ee36ba4a2b9e38e9705b68037628146e0ec50
NishantKSinha <149254763+NishantKSinha@users.noreply.github.com>
2024-09-26 23:45:59 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (8.89%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,26 +1,15 @@
 class Solution {
-    private:
-        double Pow(double x, int n) {
-         if(n == 0){
-            return 1 ;
-        }
-        return x * Pow(x , n-1);
-    }
 public:
-        double myPow(double x, int n) {
-        if (n == INT_MAX) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
-        if (n == INT_MIN) return (x == 1 || x == -1) ? 1 : 0;
-            double num = 1 ; 
-            if(n >= 0){
-                num = Pow(x , n);
-            }
-            else{
-                n = -n;
-                num = Pow(x , n);
-                num = 1.0/num;
-            }
-            return num ;
-        
+    double solve(double x , long n){
+        double ans ;
+        if(n == 0) ans = 1;
+        if(n < 0) ans = pow(1/x , -n);
+        if(n > 0) ans = pow(x,n);
+        return ans;
+    }
+       double myPow(double x, int n) {
+               
+           return solve(x,(long)n);
         
     }
 };
