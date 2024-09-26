fb2532e95a4fd94f6c9dff9127602fc9568d57e4
NishantKSinha <149254763+NishantKSinha@users.noreply.github.com>
2024-09-27 00:43:57 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (43.18%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
-    double solve(double x , long n){
-        double ans ;
-        if(n == 0) ans = 1;
-        if(n < 0) ans = pow(1/x , -n);
-        if(n > 0) ans = pow(x,n);
-        return ans;
-    }
        double myPow(double x, int n) {
-               
-           return solve(x,(long)n);
+           //recrsion
+           //tc == o (logn)
+           double ans = 1.0;    
+           long nn = n;//we take long nn bcz if we change -2^31 into positive there will be overflow
+           if(nn < 0) nn = -1*nn;
+           while(nn > 0){
+               if(nn%2 != 0){
+                   ans = ans *x;
+                   nn = nn-1;
+               }
+               else{
+                   x  = x*x;
+                   nn = nn/2;
+               }
+           }
+           if(n <  0) return double(1.0)/double(ans);
+           return ans;
         
     }
 };
