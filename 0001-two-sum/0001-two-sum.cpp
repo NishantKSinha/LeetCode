f0b2f6fdfb1f459368a5a13336c0db4f5c0e6c30
NishantKSinha <149254763+NishantKSinha@users.noreply.github.com>
2024-10-03 19:58:23 +0530

    Time: 6 ms (88.41%), Space: 14.7 MB (14.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-         vector<int> ans;
-        for(int i=0;i<nums.size()-1;i++){
-        for(int j=i+1;j<nums.size();j++){
-            if(nums[i]+nums[j]==target){
-              ans.push_back(i);
-              ans.push_back(j);
+           //TC = O(N*LOGN) == BEST CASE , WORST CASTE =- O(N*N)
+         map<int,int>mpp;
+         for(int i = 0 ; i < nums.size();i++){
+            int num = nums[i];
+            int moreneeded = target - nums[i];
+            if(mpp.find(moreneeded) != mpp.end()){
+                return {mpp[moreneeded],i};
             }
-           
+            else{       //simply put in map that element
+                 mpp[num] = i;
+            }
         }
-        }  
-    return ans;
+           return {-1,-1};
     }
         
     };
