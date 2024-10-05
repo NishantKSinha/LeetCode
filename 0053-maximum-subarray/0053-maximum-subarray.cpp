deed357b28fd55c38558c3771826e3b7e8077dbe
NishantKSinha <149254763+NishantKSinha@users.noreply.github.com>
2024-10-05 11:40:00 +0530

    Time: 64 ms (95.26%), Space: 70.7 MB (21.62%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         //kadane algorithm === tc of this soln == o(n)
-        int maxi = nums[0];
+        int maxi = INT_MIN;
         int sum = 0;
         for(int i = 0;i<nums.size();i++){
             sum = sum + nums[i];
-            if(sum >= maxi){
+            if(sum > maxi){
                 maxi = max(sum , maxi);
             }
              if(sum < 0){
