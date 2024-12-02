ab88564feec680a9d82bb4ca67bb2de1ff84372b
NishantKSinha <149254763+NishantKSinha@users.noreply.github.com>
2024-12-02 22:32:07 +0530

    Time: 0 ms (100.00%), Space: 17.1 MB (19.68%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool f(int start, vector<vector<int>>& graph, vector<int>& col){
+  /*  bool bfs(int start, vector<vector<int>>& graph, vector<int>& col){
         queue<int> q;
         q.push(start);
         col[start] = 0;
@@ -18,16 +18,34 @@ public:
             }
         }
         return true;
+    }*/
+ bool dfs(int node, vector<vector<int>>& graph, vector<int>& col) {
+        // Traverse each node and check the adjacency
+        for (auto it : graph[node]) {
+            if (col[it] == -1) { // If unvisited
+                col[it] = !col[node]; // Assign opposite color to the adjacent node
+                if (!dfs(it, graph, col)) { // Recursively visit the neighbor
+                    return false;
+                }
+            } else if (col[it] == col[node]) { // If the same color is found on adjacent nodes
+                return false;
+            }
+        }
+        return true;
     }
+
     bool isBipartite(vector<vector<int>>& graph) {
         int V = graph.size();
-        vector<int> col(V, -1);
-        for(int i = 0; i < V; i++){
-            if(col[i] == -1){
-                if(!f(i, graph, col)) return false;
+        vector<int> col(V, -1); // Initialize all vertices as uncolored (-1)
+
+        for (int i = 0; i < V; i++) { // Traverse all nodes
+            if (col[i] == -1) { // If the node is uncolored
+                col[i] = 0; // Assign an initial color (0 or 1)
+                if (!dfs(i, graph, col)) { // Start DFS from this uncolored node
+                    return false;
+                }
             }
         }
         return true;
     }
 };
-   
