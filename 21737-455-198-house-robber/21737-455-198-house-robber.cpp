30151536193ec5a1fd3ccb1099a11d8ec5b29cef
Nishant Kumar Sinha <149254763+NishantKSinha@users.noreply.github.com>
2025-01-20 18:24:27 +0530

    Time: 0 ms, Memory: 10.6 MB - LeetHub

--- 21737-455-198-house-robber/21737-455-198-house-robber.cpp
+++ 21737-455-198-house-robber/21737-455-198-house-robber.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    //tc = o(2^n)
-    int func(int i , vector<int>&nums,vector<int>&dp){
-        if(i < 0) return 0 ;
-        if(i == 0) return nums[i]; // this is not manadatory but we are manitain structure format
-
-        if(dp[i] !=-1) return dp[i];
-
-        int pick = nums[i] + func(i-2,nums,dp);
-        int notpick = 0 + func(i-1,nums,dp);
-
-        return dp[i] = max(pick,notpick);
-    }
+    //tc = o(n) === sc == o(n)+o(n)
+   
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int>dp(n,-1);
-        return func(n-1,nums,dp);
+
+       
+        
+        dp[0] = nums[0] ;
+
+        for(int i = 1 ; i < n ; i++){
+            int pick =  nums[i] ;
+            if(i > 1){
+              pick = pick + dp[i-2];
+            }   
+             int notpick = 0 + dp[i-1]; // no need to add here bcz i start from 1 and 1-1 = 0 so dont tensed
+            
+            dp[i] = max(pick,notpick); 
+        }
+        return dp[n - 1] ;
     }
 };
