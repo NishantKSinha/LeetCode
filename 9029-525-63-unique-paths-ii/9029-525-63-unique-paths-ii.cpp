9197b923b4f249384a0712d1f03768889aa9deac
Nishant Kumar Sinha <149254763+NishantKSinha@users.noreply.github.com>
2025-01-22 18:18:07 +0530

    Time: 0 ms, Memory: 11.8 MB - LeetHub

--- 9029-525-63-unique-paths-ii/9029-525-63-unique-paths-ii.cpp
+++ 9029-525-63-unique-paths-ii/9029-525-63-unique-paths-ii.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    //memoization == tc ==(m*n) == sc== (path length) + o(m*N) == (n-1)+(m-1)+O(N*M)
-    int func(int i , int j,vector<vector<int>>&dp ,vector<vector<int>>& obstacleGrid){
-    //one extra case
-    if(i >= 0 && j >=0 && obstacleGrid[i][j] == 1) return 0;
-    //base-case 
-    if(i == 0 && j ==0) return 1;
-    if(i < 0 || j < 0) return 0 ;
-
-     if(dp[i][j] != -1) return dp[i][j];
-    int left = func(i , j-1,dp,obstacleGrid);
-    int up = func(i-1 , j,dp,obstacleGrid);
-     return dp[i][j] = left + up ;
-}
+    
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
         vector<vector<int>>dp(m,vector<int>(n,-1));
 
-        return func(m-1,n-1 ,dp,obstacleGrid);
+       for(int i = 0 ; i < m;i++){
+           for(int j = 0 ; j < n ; j++){
+            if(obstacleGrid[i][j] == 1) dp[i][j] = 0 ;
+            else if(i ==0 && j == 0) dp[i][j] = 1;
+            else{
+              int left = 0;
+              int up = 0 ;  
+              if((j-1)>=0)  left = dp[i][j-1] ;
+              if((i-1) >= 0)  up = dp[i-1][j];
+              dp[i][j] = up + left;
+            }
+           }
+       }
+       return dp[m-1][n-1];
     }
 };
