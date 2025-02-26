b183964d66d31bf9b7ed13580459ce1929e2a93b
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-02-26 12:04:02 +0530

    Time: 4 ms, Memory: 13.5 MB - LeetHub

---
+++ 1782-618-899-orderly-queue/1782-618-899-orderly-queue.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:     //k=1 -> brut force ,,,, k>1 -> sort and then return the string
+    string orderlyQueue(string s, int k) {
+        
+        if(k > 1){
+            sort(s.begin(),s.end());
+            return s;
+        }
+        string result = s;
+        int n = s.length();
+        for(int i =1 ; i <= n-1 ; i++){
+            string temp = s.substr(i) + s.substr(0,i);
+            result = min(result,temp);
+        }
+        return result;
+    }
+};
