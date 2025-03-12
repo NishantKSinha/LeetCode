e268626731f82a2bda6ef6c1acec3e75ba824401
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-03-12 10:08:15 +0530

    Time: 9 ms, Memory: 16.5 MB - LeetHub

--- 6023-250-557-reverse-words-in-a-string-iii/6023-250-557-reverse-words-in-a-string-iii.cpp
+++ 6023-250-557-reverse-words-in-a-string-iii/6023-250-557-reverse-words-in-a-string-iii.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        //approach1 == by loop
-
+       /* //approach1 == by loop
         int n = s.length();
         int i = 0 ;
         while(i < n){
@@ -18,6 +17,21 @@ public:
                 i++;
             }
         }
-        return s;
+        return s;*/
+
+        //appproach2 == we will use tokenizer. tokenizer basically divide the string on basis of space or comma
+        //  etc..by default it divide on basis of space
+
+        stringstream ss(s); //it divide string into tokens
+        string token = "";
+        string result = "";
+
+        while(ss >> token){  //putting divide string one by one into another strring for operation
+              reverse(token.begin() , token.end());
+              result += token + " ";
+        }  
+
+        return result.substr(0,result.size()-1); //we have to remove last space
+
     }
 };
